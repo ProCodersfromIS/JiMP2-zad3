90c9ef4aee1d220f8ba85c02dbceb52225c9429c
BeataG <bgielbaga@gmail.com>
2014-05-21 22:13:53 +0200

    Aktualizacja 5
    dopisanie komentarzy

--- gen1.h
+++ gen1.h
@@ -5,22 +5,22 @@
 using namespace std;
 
 class gen1 : public genM {
-	int b, e, s;
-	int val;
+	int b, e, s; // b - pocz¹tek zakresu losowania e - koniec, s - ziarno
+	int val; // zwracana wylosowana liczba;
 public:
 	gen1();
-	gen1(int,int);
+	gen1(int,int); // wywo³ujesz z argumentem pocz¹tek i koniec zakresu
 	~gen1();
-	void generate();
-	int getval();
-	void seed(int);
-	int getseed();
-	void begin(int);
-	int getbegin();
-	void end(int);
-	int getend();
-	int range();
-	void show(ostream& = cout);
+	void generate(); // losowanie
+	int getval(); // zwraca wylosowana wartosc
+	void seed(int); // ustawia ziarno
+	int getseed(); // zwraca ziarno
+	void begin(int); // ustawia pocz¹tek
+	int getbegin(); // zwraca pocz¹tek
+	void end(int); // ustawia koniec
+	int getend(); // zwracaa koniec
+	int range(); // ustawia i zwraca zakres losowania
+	void show(ostream& = cout); // wyœwietla wartosc
 };
 
 #endif
